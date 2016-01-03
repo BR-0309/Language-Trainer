26a737ca6a383340f996871b451111b47126f845
BR_0309 <benjaminraison@rocketmail.com>
2016-01-03 17:24:16 +0100

    Replaced italian flag. Added following languages for translation: Lithuanian, Latvian, Asturian, Mirandese, Sardinian, Sicilian, Neapolitan.

--- src/br_0309/apps/languageTrainer/LanguageHandler.java
+++ src/br_0309/apps/languageTrainer/LanguageHandler.java
@@ -2,6 +2,7 @@ package br_0309.apps.languageTrainer;
 
 import java.util.Locale;
 
+// TODO: Switch to ISO 3-letter codes
 public class LanguageHandler {
 
     public final static Locale[] INTERFACE_LANGS = {Locale.UK, Locale.GERMANY};
@@ -9,40 +10,25 @@ public class LanguageHandler {
      * SCOT, WAL and INT are made up to represent Scotland, Wales and
      * international, respectively
      */
-    public static final Locale[] LANGUAGES = {new Locale("af", "ZA"), new Locale("an", "ES"), new Locale("ay", "BO"),
-                                              new Locale("bi", "VU"), new Locale("br", "FR"), new Locale("ca", "ES"),
-                                              new Locale("ch", "US"),
-                                              new Locale("co", "FR"), new Locale("cs", "CZ"), new Locale("da", "DK"),
-                                              Locale.UK, new Locale("eo", "INT"),
-                                              new Locale("et", "EE"), new Locale("eu", "ES"), new Locale("ff", "INT"),
-                                              new Locale("gl", "ES"),
-                                              Locale.GERMANY,
-                                              new Locale("gn", "PY"), new Locale("ht", "HT"), new Locale("ho", "PG"),
-                                              new Locale("ia", "INT"),
-                                              new Locale("ie", "INT"),
-                                              new Locale("fi", "FI"), Locale.FRANCE, new Locale("fj", "FJ"),
-                                              new Locale("fo", "FO"),
-                                              new Locale("hr", "HR"), new Locale("io", "INT"), new Locale("is", "IS"),
-                                              new Locale("it", "IT"),
-                                              new Locale("kg", "CD"), new Locale("la", "INT"), new Locale("lb", "LU"),
-                                              new Locale("mi", "NZ"),
-                                              new Locale("mg", "MG"), new Locale("kw", "GB"), new Locale("mt", "MT"),
-                                              new Locale("gv", "GB"),
-                                              new Locale("nb", "NO"), new Locale("nn", "NO"), new Locale("no", "NO"),
-                                              new Locale("oc", "FR"),
-                                              new Locale("pl", "PL"), new Locale("pt", "PT"), new Locale("nl", "NL"),
-                                              new Locale("rm", "CH"),
-                                              new Locale("ro", "RO"), new Locale("sm", "US"), new Locale("gd", "SCOT"),
-                                              new Locale("sk", "SK"),
-                                              new Locale("sl", "SL"), new Locale("st", "LS"), new Locale("es", "ES"),
-                                              new Locale("sq", "AL"),
-                                              new Locale("sw", "KE"), new Locale("sv", "SE"), new Locale("ss", "SZ"),
-                                              new Locale("tn", "BW"),
-                                              new Locale("to", "TO"), new Locale("tr", "TR"), new Locale("ve", "ZA"),
-                                              new Locale("vo", "INT"),
-                                              new Locale("wa", "BE"), new Locale("cy", "WAL"), new Locale("wo", "SN"),
-                                              new Locale("xh", "ZA"),
-                                              new Locale("zu", "ZA")};
+    public static final Locale[] LANGUAGES = {new Locale("af", "ZA"), new Locale("an", "ES"), new Locale("ay", "BO"), new Locale("bi", "VU"),
+                                              new Locale("br", "FR"), new Locale("ca", "ES"), new Locale("ch", "US"), new Locale("co", "FR"),
+                                              new Locale("cs", "CZ"), new Locale("cy", "WAL"), new Locale("da", "DK"), new Locale("de", "DE"),
+                                              new Locale("en", "GB"), new Locale("eo", "INT"), new Locale("es", "ES"), new Locale("et", "EE"),
+                                              new Locale("eu", "ES"), new Locale("ff", "INT"), new Locale("fi", "FI"), new Locale("fj", "FJ"),
+                                              new Locale("fo", "FO"), new Locale("fr", "FR"), new Locale("ga", "IE"), new Locale("gd", "SCOT"),
+                                              new Locale("gl", "ES"), new Locale("gn", "PY"), new Locale("gv", "GB"), new Locale("ho", "PG"),
+                                              new Locale("hr", "HR"), new Locale("ht", "HT"), new Locale("ia", "INT"), new Locale("ie", "INT"),
+                                              new Locale("io", "INT"), new Locale("is", "IS"), new Locale("it", "IT"), new Locale("kg", "CD"),
+                                              new Locale("kw", "GB"), new Locale("la", "INT"), new Locale("lb", "LU"), new Locale("mg", "MG"),
+                                              new Locale("mi", "NZ"), new Locale("mt", "MT"), new Locale("nb", "NO"), new Locale("nl", "NL"),
+                                              new Locale("nn", "NO"), new Locale("no", "NO"), new Locale("oc", "FR"), new Locale("pl", "PL"),
+                                              new Locale("pt", "PT"), new Locale("rm", "CH"), new Locale("ro", "RO"), new Locale("sk", "SK"),
+                                              new Locale("sl", "SL"), new Locale("sm", "US"), new Locale("sq", "AL"), new Locale("ss", "SZ"),
+                                              new Locale("st", "LS"), new Locale("sv", "SE"), new Locale("sw", "KE"), new Locale("tn", "BW"),
+                                              new Locale("to", "TO"), new Locale("tr", "TR"), new Locale("ve", "ZA"), new Locale("vo", "INT"),
+                                              new Locale("wa", "BE"), new Locale("wo", "SN"), new Locale("xh", "ZA"), new Locale("zu", "ZA"),
+                                              new Locale("lt", "LT"), new Locale("lv", "LV"), new Locale("ast", "ES"), new Locale("mwl", "PT"),
+                                              new Locale("srd", "IT"), new Locale("scn", "IT"), new Locale("nap", "IT")};
 
     /**
      * @return the best suited locale given the current system language and the
@@ -87,8 +73,7 @@ public class LanguageHandler {
     }
 
     public static String getFlag(Locale locale) {
-        String country = locale.getCountry().trim();
-        if (!country.isEmpty()) {
+        String country = locale.getCountry().trim(); if (! country.isEmpty()) {
             return "/icons/flags/" + country + ".png";
         } else {
             for (Locale l : LANGUAGES) {
