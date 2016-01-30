94b6fb6f0a212780ca2393f9797d7634aea68d12
BR_0309 <benjaminraison@rocketmail.com>
2016-01-30 21:55:14 +0100

    Commit to add to the messages of today that I added a "total" field to statistics to prevent cheating

--- src/br_0309/apps/languageTrainer/Reference.java
+++ src/br_0309/apps/languageTrainer/Reference.java
@@ -34,7 +34,7 @@ public class Reference {
     public static final String DEFAULT_PROFILE_DIR = SystemUtil.getUserHome();
 
     public static final String LOGO = "/icons/Logo.png";
-    public static final String VERSION = "0.6.0";
+    public static final String VERSION = "0.6.5";
 
     public static final String[] SOUNDS_CORRECT = {"/sounds/correct/correct.wav", "/sounds/correct/correct2.wav", "/sounds/correct/game-sound-correct-v2.wav",
                                                    "/sounds/correct/jingle-4.wav", "/sounds/correct/powerup-success.wav"};
